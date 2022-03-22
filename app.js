69258c64c961e4423b8683be6e76a38916e7e80a
Pete Hamrick <pete.hamrick@gmail.com>
2022-03-22 16:09:17 -0700

    wrote displayGoblins() function to dynamically display goblins from state

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ import { renderGoblin } from './render-utils.js';
 const goblinListEl = document.querySelector('.enemies-container');
 
 // let state
-let goblin = {
-    hp: 0,
-};
+let goblins = [{ hp: 0 }, { hp: 4 }, { hp: 3 }, { hp: 2 }, { hp: 1 }];
 
 // set event listeners
 
-const goblinEl = renderGoblin(goblin);
-
-goblinListEl.append(goblinEl);
 // get user input
 // use user input to update state
 // update DOM to reflect the new state
+
+function displayGoblins() {
+    for (let goblin of goblins) {
+        const goblinEl = renderGoblin(goblin);
+
+        goblinListEl.append(goblinEl);
+    }
+}
+
+displayGoblins();
